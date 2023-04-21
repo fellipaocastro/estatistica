efe6516f57fb81d8f53ce1936d7dc0be71e719bf
Fellipe Castro <contact@fellipecastro.com>
2023-04-20 22:35:31 -0300

    Update

--- amostragem.py
+++ amostragem.py
@@ -2,6 +2,7 @@ import random
 
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import StratifiedShuffleSplit
 
 
 def amostragem_aleatoria_simples(dataset, amostras, seed=1):
@@ -40,6 +41,15 @@ def amostragem_agrupamento(dataset, numero_grupos, seed=1):
     return dataset[dataset['grupo'] == grupo_selecionado]
 
 
+def amostragem_estratificada(dataset, amostras, seed=1):
+    percentual = amostras / len(dataset)
+    split = StratifiedShuffleSplit(test_size=percentual, random_state=seed)
+
+    _, test_index = next(split.split(dataset, dataset['income']))
+
+    return dataset.iloc[test_index]
+
+
 if __name__ == '__main__':
     ds_census = pd.read_csv('datasets/census.csv')
 
@@ -58,3 +68,9 @@ if __name__ == '__main__':
     print(df_amostra_agrupamento.shape)
     print(df_amostra_agrupamento['grupo'].value_counts())
     print(df_amostra_agrupamento)
+
+    print('\nAmostragem estratificada')
+    df_amostra_estratificada = amostragem_estratificada(ds_census, 100)
+    print(df_amostra_estratificada.shape)
+    print(df_amostra_estratificada['income'].value_counts())
+    print(df_amostra_estratificada)
